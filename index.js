6f53592d93379aeba3867e0bb11e61f4e2144013
luciferx6 <luciferx6@gmail.com>
2021-05-04 14:23:12 +0500

    Console

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const fs = require("fs/promises");
 
-const file = fs.readFile("sample.json");
-const read = JSON.parse(file.toString());
+setInterval(async () => {
+  const file = await fs.readFile("sample.json");
+  const obj = JSON.parse(file.toString());
+  console.log(obj.phoneNumbers[0].number);
+  console.log(obj.firstName, obj.lastName);
+  console.log(obj.address.streetAddress, obj.address.city, obj.address.state);
+}, 1000);
 
-const interval = (ms) =>
-  new Promise((resolve, reject) => {
-    setInterval(() => {
-      resolve();
-      console.log(read.firstName);
-    }, ms);
-  });
+// const file = fs.readFile("sample.json");
+// const read = JSON.parse(file.toString());
 
-const main = async () => {
-  interval(500);
-  const json = await fs.readFile("sample.json");
-  console.log(json);
-};
+// const interval = (ms) =>
+//   new Promise((resolve, reject) => {
+//     setInterval(() => {
+//       resolve();
+//       console.log(read.firstName);
+//     }, ms);
+//   });
 
-main();
+// const main = async () => {
+// setInterval
+// };
+
+// main();
 
 console.log("END");
